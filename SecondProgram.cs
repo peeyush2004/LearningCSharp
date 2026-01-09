3bee6dd756c2251b75612eb70e292fd3e3c11af5
peeyush2004 <peeyush.patil0718@gmail.com>
2026-01-09 20:56:04 +0530

    Learned new things in c# that unlike java it accepts the value in String format so we need to use Parse method to convert from string to int or any kind of data type it can be string to double, short etc. Second thing that we need to use Console.ReadKey() method so that the console screen will remain open even after the program is fully executed or else the console closes itself as soon as any key is pressed.

---
+++ SecondProgram.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Hello World");
+string name1 = "Peeyush";
+Console.WriteLine(name1);
+
+int name2 = int.Parse(Console.ReadLine());
+Console.WriteLine(name2);
+
+Console.ReadKey();
