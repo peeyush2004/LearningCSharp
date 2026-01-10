d0c954271b0f580f96d25b57d4165a90814a0844
peeyush2004 <peeyush.patil0718@gmail.com>
2026-01-10 23:50:46 +0530

    String Interpolation concept in c# helps in displaying one of the cleanest and most popular implementations, which is why it often feels “C#-only”. String interpolation means embedding variables or expressions directly inside a string instead of concatenating them.

---
+++ StringInterpolation.cs
@@ -0,0 +1,6 @@
+//Understanding String Interpolation
+string name = "Peeyush";
+int age = 22; 
+
+// Using string interpolation to format the output
+Console.WriteLine($"Hello,My name is {name}! and I am {age} years old.");
